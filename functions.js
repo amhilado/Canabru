58b57154cca1590eea8b4280b77efc6f94084f04
Aaron H <ahilado@uci.edu>
2019-10-21 11:13:40 -0700

    Temporary Fix for Responsive Nav

---
+++ functions.js
@@ -0,0 +1,22 @@
+/** Source: https://www.w3schools.com/howto/howto_js_dropdown.asp **/
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.fa-bars')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
